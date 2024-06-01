b448ab68ba875c78c98cc2926b42aca187232314
Majed Karimi <m.karimi@avakatan.ir>
2024-06-01 20:29:30 +0430

    create post page

--- src/app/create-posts/page.tsx
+++ src/app/create-posts/page.tsx
@@ -1,12 +1,21 @@
+import { createPost } from "@/actions/action";
 import React from "react";
 
 const page = () => {
   return (
     <main className="text-center pt-16">
       <h1 className="text-4xl md:text:5xl font-bold mb-5">Create Post</h1>
-      <form className="h-10 space-x-2 mt-10" action="">
-        <input className="border rounded px-3 h-full" type="text" />
-        <button>Submit</button>
+      <form className="h-10 space-x-2 mt-10">
+        <input
+          className="border rounded px-3 h-full "
+          type="text"
+          name="title"
+          placeholder="Title for new post"
+          required
+        />
+        <button className="h-full bg-blue-500 px-5 rounded text-white">
+          Submit
+        </button>
       </form>
     </main>
   );
